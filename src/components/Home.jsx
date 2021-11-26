8f91cacb45457b10c981a323190e60b4c651b560
unknown <ermolov1337@gmail.com>
2021-11-26 13:51:02 +0300

    add picture with text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Card, Col, Container, Row, Button } from "react-bootstrap";
 import Slider from "./Slider";
 import ocean from '../img/ocean.jfif'
+import Jumbotron from "./Jumbotron";
+import ocean1 from '../img/ocean1.jpg'
 
 
 
@@ -10,7 +12,7 @@ export default function Home() {
 
         <>
             <Slider />
-            <Container style={{ paddingTop: "2rem", paddingBottom:"2rem" }}>
+            <Container style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
 
                 <Row>
                     <Col>
@@ -22,21 +24,6 @@ export default function Home() {
                                     Quas suscipit ullam maiores. Odit, blanditiis praesentium!
                                     Deleniti accusantium eaque ipsa,
                                     nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
-                                    </Card.Text>
-                                        <Button variant="primary">Узнать подробнее </Button>
-                                </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col>
-                        <Card style={{ width: "18rem" }}>
-                            <Card.Img variant="top" src={ocean} />
-                            <Card.Body>
-                                <Card.Title>Тест карточки</Card.Title>
-                                <Card.Text> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                                    Quas suscipit ullam maiores. Odit, blanditiis praesentium!
-                                    Deleniti accusantium eaque ipsa,
-                                    nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
                                 </Card.Text>
                                 <Button variant="primary">Узнать подробнее </Button>
                             </Card.Body>
@@ -72,8 +59,50 @@ export default function Home() {
                             </Card.Body>
                         </Card>
                     </Col>
-                    
 
+                    <Col>
+                        <Card style={{ width: "18rem" }}>
+                            <Card.Img variant="top" src={ocean} />
+                            <Card.Body>
+                                <Card.Title>Тест карточки</Card.Title>
+                                <Card.Text> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Quas suscipit ullam maiores. Odit, blanditiis praesentium!
+                                    Deleniti accusantium eaque ipsa,
+                                    nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
+                                </Card.Text>
+                                <Button variant="primary">Узнать подробнее </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                </Row>
+            </Container>
+
+            <Container style={{marginBottom: "30px"}}>
+                <Row>
+                    <Col md={7}>
+                        <img src={ocean1} height={400} />
+                        
+                    </Col>
+
+
+                    <Col md={5}>
+                        <h2>Выносим Текст с Карточкой - пример!</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas suscipit ullam maiores. Odit, blanditiis praesentium!
+                            Deleniti accusantium eaque ipsa,
+                            nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas suscipit ullam maiores. Odit, blanditiis praesentium!
+                            Deleniti accusantium eaque ipsa,
+                            nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas suscipit ullam maiores. Odit, blanditiis praesentium!
+                            Deleniti accusantium eaque ipsa,
+                            nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
+                        </p>
+                    </Col>
                 </Row>
             </Container>
         </>
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+// import React from "react";
+// import { Jumbotron as Jumbo, Container } from "react-bootstrap";
+// import ocean from '../img/ocean.jfif'
+// import styled from "styled-components";
 
-export default function About() {
-    return (
+// const Styles = styled.div`
+// .jumbo {
+//     background: url(${ocean} no repeat fixed bottom;
+//     background-size: cover;
+//     color: #efefef;
+//     height: 400px;
+//     position: relative;
+//     z-index:-2;
+// }
+// `;
 
-        <>
-            <h1> page About </h1>
-        </>
-    )
-}
+
+// const Jumbotron = () => {
+//     return (
+//         <Styles>
+//             <Jumbo fluid className="jumbo">
+//                 <div className="overlay"> </div>
+//                 <Container>
+//                     <h1>Полёт отлинчый</h1>
+//                     <p>
+//                         Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+//                         Quas suscipit ullam maiores. Odit, blanditiis praesentium!
+//                         Deleniti accusantium eaque ipsa,
+//                         nihil deserunt ad quidem quae expedita ducimus sunt id, voluptas animi.
+
+//                     </p>
+//                 </Container>
+//             </Jumbo>
+//         </Styles>
+//     )
+// }
+
+// export default Jumbotron;
