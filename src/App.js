d63e1941c47e76761680ec15add3abd57a1ae725
unknown <ermolov1337@gmail.com>
2021-11-26 14:03:07 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import  NaviBar  from './components/Navibar.jsx';
 import User from './components/User';
@@ -18,6 +19,7 @@ function App() {
           <Route path='/users' element={<User />} />
           <Route path='/about' element={<About />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
       </div>
       
